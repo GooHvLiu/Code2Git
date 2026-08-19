f36eba94f39f9205ab49cacb9abc89bedc16f3aa
GooHvLiu <879639340@qq.com>
2026-08-19 18:44:17 +0800

    更新模板

--- 03-Project_00/02-nex/02.02-nexSM/02.02.02-nexSM-V2/test/sql/firstLogin.sql
+++
@@ -1,6 +0,0 @@
-USE nexsm_v2_dev;
-
--- 增加是否首次登录 tinyint(1)，默认1：新用户默认是首次登录
-ALTER TABLE nex_user 
-ADD COLUMN is_first_login TINYINT(1) NOT NULL DEFAULT 1 COMMENT '是否首次登录 1是 0否',
-ADD COLUMN first_login_at DATETIME NULL COMMENT '首次登录时间';
